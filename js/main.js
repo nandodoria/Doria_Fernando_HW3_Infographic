fd13fc2a0df634eb404237b10736e082673bb0ee
nandodoria <nandodoria@hotmail.ca>
2018-10-26 21:25:27 -0400

    adding new graph svg code and adding first js animations

--- js/main.js
+++ js/main.js
@@ -1,18 +1,60 @@
 (() => {
 
-    let graphimage = document.querySelector("#graph_image");
 
-    function animateGraph() {
-        TweenMax.to(graphimage, 1, {x: 300, rotation: 120, xScale: 1.1, yScale: 1.1});
+    
+    let photothumb = document.querySelector("#photo_x5F_icon");
+    let bluecircle = document.querySelector("#blue_x5F_bubble")
+    let orangecircle = document.querySelector("#orange_x5F_bubble")
+    let greencircle = document.querySelector("#green_x5F_bubble")
+    
+
+    function changecolour(){
+        photothumb.style.opacity = "0.5";
     }
 
-    function unAnimateGraph(){
-        TweenMax.to(graphimage, 1, {x: 0, y:0, rotation: 0, xScale: 1, yScale: 1});
+    function changecolourback(){
+        photothumb.style.opacity = "1";
+    }
+
+    function circle1grow(){
+        TweenMax.to(bluecircle, 0.5, {scale: 0.8, ease:Back.easeOut});
+    }
+
+    function circle1shrink(){
+        TweenMax.to(bluecircle, 0.5, {scale: 1, ease:Bounce.easeOut});
+    }
+
+    function circle2grow(){
+        TweenMax.to(orangecircle, 0.5, {scale: 0.8, ease:Back.easeOut});
+    }
+
+    function circle2shrink(){
+        TweenMax.to(orangecircle, 0.5, {scale: 1, ease:Bounce.easeOut});
+    }
+
+    function circle3grow(){
+        TweenMax.to(greencircle, 0.5, {scale: 0.8, ease:Back.easeOut});
+    }
+
+    function circle3shrink(){
+        TweenMax.to(greencircle, 0.5, {scale: 1, ease:Bounce.easeOut});
     }
 
 
-    graphimage.addEventListener("mouseover", animateGraph);
-    graphimage.addEventListener("mouseout", unAnimateGraph);
+    photothumb.addEventListener("mouseover", changecolour);
+    photothumb.addEventListener("mouseout", changecolourback);
 
-	
+    bluecircle.addEventListener("mouseover", circle1grow);
+    bluecircle.addEventListener("mouseout", circle1shrink);
+
+    orangecircle.addEventListener("mouseover", circle2grow);
+    orangecircle.addEventListener("mouseout", circle2shrink);
+
+    greencircle.addEventListener("mouseover", circle3grow);
+    greencircle.addEventListener("mouseout", circle3shrink);
+    
+
+
+
+
 })();
