c3e4340f62f55279da943ec7d6b26e30033b6f0a
nandodoria <nandodoria@hotmail.ca>
2018-10-28 20:37:10 -0400

    adding new animations

--- js/main.js
+++ js/main.js
@@ -9,8 +9,14 @@
 
     let worldthumb = document.querySelector("#world_x5F_icon");
     let worldnotif = document.querySelector("#notif2");
-    
 
+    let pulse0 = document.querySelector("#pulse0");
+    let pulse1 = document.querySelector("#pulse1");
+    let pulse2 = document.querySelector("#pulse2");
+    let pulse3 = document.querySelector("#pulse3");
+    let pulse4 = document.querySelector("#pulse4");
+    let pulse5 = document.querySelector("#pulse5");
+    
 
     let bluecircle = document.querySelector("#blue_x5F_bubble");
     let orangecircle = document.querySelector("#orange_x5F_bubble");
@@ -19,7 +25,7 @@
     
 
     function circle1grow(){
-        TweenMax.to(bluecircle, 0.5, {scale: 0.8, ease:Back.easeOut});
+        TweenMax.to(bluecircle, 0.5, {scale: 1.2, transformOrigin:"20% 50%", ease:Back.easeOut});
     }
 
     function circle1shrink(){
@@ -27,7 +33,7 @@
     }
 
     function circle2grow(){
-        TweenMax.to(orangecircle, 0.5, {scale: 0.8, ease:Back.easeOut});
+        TweenMax.to(orangecircle, 0.5, {scale: 1.2, transformOrigin:"50% 50%", ease:Back.easeOut});
     }
 
     function circle2shrink(){
@@ -35,7 +41,7 @@
     }
 
     function circle3grow(){
-        TweenMax.to(greencircle, 0.5, {scale: 0.8, ease:Back.easeOut});
+        TweenMax.to(greencircle, 0.5, {scale: 1.2, transformOrigin:"80% 50%", ease:Back.easeOut});
     }
 
     function circle3shrink(){
@@ -57,6 +63,23 @@
     
     }
 
+
+
+    function pulse0Grow(){
+        TweenMax.to(pulse0, 0.4, {scale:1.5, transformOrigin:"50% 50%", ease:Back.easeOut});
+    }
+
+    function pulse0Shrink(){
+        TweenMax.to(pulse0, 0.4, {scale:1, transformOrigin:"50% 50%", ease:Bounce.easeOut});
+    }
+
+    function pulse1Grow(){
+        TweenMax.to(pulse1, 0.4, {scale:1.5, transformOrigin:"50% 50%", ease:Back.easeOut});
+    }
+
+    function pulse1Shrink(){
+        TweenMax.to(pulse1, 0.4, {scale:1, transformOrigin:"50% 50%", ease:Bounce.easeOut});
+    }
     
 
 
@@ -73,6 +96,24 @@
     photothumb.addEventListener("click", showText1)
     worldthumb.addEventListener("click", showText2);
     pizzathumb.addEventListener("click", showText3);
+
+
+
+    pulse0.addEventListener("mouseover", pulse0Grow);
+    pulse0.addEventListener("mouseout", pulse0Shrink);
+    pulse1.addEventListener("mouseover", pulse1Grow);
+    pulse1.addEventListener("mouseout", pulse1Shrink);
+    pulse2.addEventListener("mouseover", pulse2Grow);
+    pulse2.addEventListener("mouseout", pulse2Shrink);
+    pulse3.addEventListener("mouseover", pulse3Grow);
+    pulse3.addEventListener("mouseout", pulse3Shrink);
+    pulse4.addEventListener("mouseover", pulse4Grow);
+    pulse4.addEventListener("mouseout", pulse4Shrink);
+    pulse5.addEventListener("mouseover", pulse5Grow);
+    pulse5.addEventListener("mouseout", pulse5Shrink);
+    pulse6.addEventListener("mouseover", pulse6Grow);
+    pulse6.addEventListener("mouseout", pulse6Shrink);
+
     
 
 
